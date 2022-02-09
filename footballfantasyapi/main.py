4bad2253da8cf5a857e9f268399f16f8eb870252
yousef-syntheia-at-731135317831 <Fw8zpLtqTstnRtn/ePQ/yWUIm/7qKWaT2hblQAb58tA=>
2022-02-09 17:44:40 +0200

    Remove root endpoint

--- footballfantasyapi/main.py
+++ footballfantasyapi/main.py
@@ -1,31 +1,11 @@
 import time
-from functools import lru_cache
-from sqlalchemy.orm import Session
-from fastapi import FastAPI, APIRouter, Depends, HTTPException, Request
+from fastapi import FastAPI, APIRouter, Request
 
-from footballfantasyapi import crud
-from footballfantasyapi.api import deps
 from footballfantasyapi.core.config import settings
 from footballfantasyapi.api.api_v1.api import api_router
 
 root_router = APIRouter()
 app = FastAPI(title="Fantasy Football API")
-
-
-# @lru_cache()
-# def get_settings():
-#     return settings()
-
-
-@root_router.get("/", status_code=200)
-def root(
-        request: Request,
-        db: Session = Depends(deps.get_db)
-) -> dict:
-    """
-    Root GET
-    """
-    return crud.team.get_multi(db=db)
 
 
 @app.middleware("http")
